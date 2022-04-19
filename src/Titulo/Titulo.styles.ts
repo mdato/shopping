9d7951a9bd4c681e64ef19535f6bc6d22ba18973
mdato <85354798+mdato@users.noreply.github.com>
2022-04-19 16:14:44 -0300

    Add files via upload

---
+++ src/Titulo/Titulo.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.aside`
+  font-family: Arial, Helvetica, sans-serif;
+  max-width: 200px;
+  padding: 10px;
+  position: fixed;
+  overflow: hidden;
+`;
---
+++ src/Titulo/Titulo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+//import Badge from "@material-ui/core/Badge";
+import { Wrapper } from './Titulo.styles'
+
+export default function Titulo() {
+  return (
+    <div className="App">
+      <div className="waviy">
+        <span className="--i:1">B</span>
+        <span className="--i:2">O</span>
+        <span className="--i:3">D</span>
+        <span className="--i:4">I</span>
+        <span className="--i:5">E</span>
+        <span className="--i:6">-</span>
+        <span className="--i:7">S</span>
+        <span className="--i:8">H</span>
+        <span className="--i:9">O</span>
+        <span className="--i:10">P</span>
+
+      </div>
+      
+    </div>
+  );
+}
