0024637d0e00a626e520e0e9c5b735626c4d6682
mdato <85354798+mdato@users.noreply.github.com>
2022-04-19 16:17:15 -0300

    Add files via upload

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.aside`
+  font-family: 'Poppins', sans-serif;
+  width: 280px;
+  padding: 10px;
+`;
---
+++ src/Cart/Cart.tsx
@@ -0,0 +1,33 @@
+import CartItem from '../CartItem/CartItem';
+import { Wrapper } from './Cart.styles';
+import { CartItemType } from '../App';
+
+type Props = {
+  cartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+
+  return (
+    <Wrapper>
+
+      <h4>Total: ${calculateTotal(cartItems).toFixed(2)}</h4>
+      {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+      {cartItems.map(item => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+
+    </Wrapper>
+  );
+};
+
+export default Cart;
